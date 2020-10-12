f0cff9dd65cec4b8adebbc275216a5a30a5277ff
Kazuhiro Fujieda <fujieda@roundwide.com>
2020-10-12 14:46:15 +0900

    Add DynamicJson class for full compatilibity

---
+++ DynaJson/DynamicJson.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Text;
+
+namespace DynaJson
+{
+    public class DynamicJson
+    {
+        public static dynamic Parse(string json)
+        {
+            return JsonObject.Parse(json);
+        }
+
+        public static dynamic Parse(Stream stream)
+        {
+            return JsonObject.Parse(stream);
+        }
+
+        public static dynamic Parse(Stream stream, Encoding encoding)
+        {
+            return JsonObject.Parse(stream, encoding);
+        }
+
+        public static string Serialize(object obj)
+        {
+            return JsonObject.Serialize(obj);
+        }
+    }
+}
