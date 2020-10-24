5543bcb31e2c2484e64b4ef4d4d54b906aad8441
Kazuhiro Fujieda <fujieda@roundwide.com>
2020-10-24 21:54:34 +0900

    Make TypeDictionary thread safe

--- DynaJson/TypeDictionary.cs
+++ DynaJson/TypeDictionary.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
+using System.Threading;
 
 namespace DynaJson
 {
@@ -17,16 +18,20 @@ namespace DynaJson
             }
         }
 
+        private int _working;
         private int _count = -1;
         private KeyValuePair[] _list = new KeyValuePair[5];
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public T Insert(Type key, T value)
         {
+            if (Interlocked.Exchange(ref _working, 1) != 0)
+                return value;
             _count++;
             if (_count >= _list.Length)
                 Array.Resize(ref _list, _list.Length * 2);
             _list[_count] = new KeyValuePair(key, value);
+            Interlocked.Exchange(ref _working, 0);
             return value;
         }
 
